11a9e14f23a8f53e62de6166fc2f0a5da361a949
danbolt <dansvg@gmail.com>
2012-09-20 18:53:50 -0700

    Added base class and new PNG
    Made a quick foundation for the Entity class to work on with Eric this
    evening.

---
+++ MagnetBoy/MagnetBoy/MagnetBoy/Entity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MagnetBoy
+{
+    class Entity
+    {
+        //sexy class data for our game entity goes here
+
+        virtual void update(GameTime currentTime)
+        {
+            return;
+        }
+
+        virtual void draw(SpriteBatch sb)
+        {
+            return;
+        }
+    }
+}
