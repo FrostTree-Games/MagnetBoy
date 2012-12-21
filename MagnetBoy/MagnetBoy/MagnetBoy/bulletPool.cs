ab020835c546281a5d9a6b6a98ba5eb099286454
danbolt <dansvg@gmail.com>
2012-12-21 14:41:21 -0800

    inital layout for projectile engine

--- MagnetBoy/MagnetBoy/MagnetBoy/bulletPool.cs
+++ MagnetBoy/MagnetBoy/MagnetBoy/bulletPool.cs
@@ -2,10 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace MagnetBoy
 {
-    class bulletPool
+    class Bullet : Entity
     {
+        private bool inUse = false;
+        private double startTime = 0;
+        private double maxLifeTime = 5000; 
+        private double rotation = 0.0; // 0.0 in rotation is considered to be right-facing, or "EAST"
+
+        public void add(float newX, float newY, BulletPool.BulletType type, GameTime entryTime)
+        {
+            inUse = true;
+            startTime = entryTime.ElapsedGameTime.Milliseconds;
+
+            horizontal_pos = newX;
+            vertical_pos = newY;
+        }
+
+        public override void update(GameTime currentTime)
+        {
+            // bullet functionality
+
+            if (currentTime.ElapsedGameTime.Milliseconds - startTime > maxLifeTime)
+            {
+                inUse = false;
+            }
+        }
+
+        public override void draw(SpriteBatch sb)
+        {
+            //
+        }
+    }
+
+    class BulletPool
+    {
+        public enum BulletType
+        {
+            Laser,
+            Rocket
+        }
+
+        private Bullet[] pool = null;
+
+        public BulletPool()
+        {
+            pool = new Bullet[15];
+        }
+
+        public void updatePool(GameTime currentTime)
+        {
+            //
+        }
+
+        public void pushBullet(GameTime currentTime)
+        {
+            //
+        }
     }
 }
