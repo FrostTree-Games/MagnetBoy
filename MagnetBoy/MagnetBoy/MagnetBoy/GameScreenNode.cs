1c2086d3cf5579517e21bf58cb03f0c9081e94b1
danbolt <dansvg@gmail.com>
2012-12-27 15:13:12 -0800

    groundwork for GameScreenNode

---
+++ MagnetBoy/MagnetBoy/MagnetBoy/GameScreenNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/* The GameScreenNode class is used to contain and organize various "scenes"
+ * of the game. A scene is meant to represent a distinct area of content, such
+ * as a title screen, level select, ingame level, etc.
+ */
+
+namespace MagnetBoy
+{
+    abstract class GameScreenNode
+    {
+        private bool isUpdateable = true;
+        public bool IsUpdateable
+        {
+            get
+            {
+                return isUpdateable;
+            }
+            set
+            {
+                isUpdateable = value;
+            }
+        }
+
+
+    }
+}
