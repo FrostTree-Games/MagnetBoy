4bd403dea80e1b5ca0292b54711fd08accce18fd
Eric Chan <ettchan618@gmail.com>
2013-01-12 15:41:41 -0800

    Angry Saw death animation done

--- MagnetBoy/MagnetBoy/MagnetBoy/Player.cs
+++ MagnetBoy/MagnetBoy/MagnetBoy/Player.cs
@@ -334,7 +334,8 @@ namespace MagnetBoy
                         if (!onTheGround && velocity.Y > 0.001f && vertical_pos < en.Position.Y)
                         {
                             velocity.Y *= -1.1f;
-                            en.removeFromGame = true;
+                            //en.removeFromGame = true;
+                            en.deathAnimation = true;
                             //en.death();
                         }
                     }
