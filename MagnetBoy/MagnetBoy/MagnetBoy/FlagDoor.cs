a86730b773e41b0f98f11955685e20ede3b7e148
Daniel Savage <dansvg@gmail.com>
2013-01-17 11:19:57 -0800

    flag-based doors

--- MagnetBoy/MagnetBoy/MagnetBoy/FlagDoor.cs
+++ MagnetBoy/MagnetBoy/MagnetBoy/FlagDoor.cs
@@ -11,6 +11,9 @@ namespace MagnetBoy
     {
         private LevelState.FlagColor doorColor;
 
+        private const double doorClickDelay = 300;
+        private double doorClickDelta = 0;
+
         private const float doorMaxHeight = 64.0f;
         private int doorHeight; //based on 8-pixel-high blocks
         public int DoorHeight
@@ -46,12 +49,38 @@ namespace MagnetBoy
 
         public override void update(GameTime currentTime)
         {
-            //foo
+            if (!doorIsClosed)
+            {
+                if (doorHeight > 0)
+                {
+                    doorClickDelta += currentTime.ElapsedGameTime.Milliseconds;
+
+                    if (doorClickDelta >= doorClickDelay)
+                    {
+                        doorClickDelta = 0;
+                        doorHeight -= 1;
+                        height = 32f + (8f * doorHeight);
+                    }
+                }
+            }
+
+            if (doorIsClosed && LevelState.getFlag(doorColor))
+            {
+                doorClickDelta = 0;
+            }
+
+            doorIsClosed = !(LevelState.getFlag(doorColor));
         }
 
         public override void draw(SpriteBatch sb)
         {
-            AnimationFactory.drawAnimationFrame(sb, "flagDoor", 0, Position);
+            AnimationFactory.drawAnimationFrame(sb, "flagDoor", 0, Position, LevelState.getFlagXNAColor(doorColor));
+
+            for (int i = 0; i < doorHeight; i++)
+            {
+                Vector2 pos = Position + new Vector2(0, 32 + (i * 8f));
+                AnimationFactory.drawAnimationFrame(sb, "flagDoorWall", 0, pos);
+            }
         }
     }
 }
