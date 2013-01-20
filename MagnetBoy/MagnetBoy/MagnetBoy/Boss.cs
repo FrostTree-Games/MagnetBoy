73337862f8da5290514c9137a30afd4e5fe1c701
Eric Chan <ettchan618@gmail.com>
2013-01-19 17:47:55 -0800

    Boss code

--- MagnetBoy/MagnetBoy/MagnetBoy/Boss.cs
+++ MagnetBoy/MagnetBoy/MagnetBoy/Boss.cs
@@ -91,8 +91,11 @@ namespace MagnetBoy
 
                 currentFrame = (currentFrame + 1) % AnimationFactory.getAnimationFrameCount(currentAnimation);
             }
+<<<<<<< HEAD
+=======
 
 
+>>>>>>> origin/Eric-Dev
             return;
         }
 
