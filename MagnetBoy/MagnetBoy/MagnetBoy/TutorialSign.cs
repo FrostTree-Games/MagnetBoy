3b81e24d186176f81b455a08a83fbcbe1ffd5707
Daniel Savage <dansvg@gmail.com>
2013-01-19 17:38:33 -0800

    tutorial sign base added

---
+++ MagnetBoy/MagnetBoy/MagnetBoy/TutorialSign.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MagnetBoy
+{
+    class TutorialSign : Entity
+    {
+        public TutorialSign(float newX, float newY)
+        {
+            //
+        }
+    }
+}
