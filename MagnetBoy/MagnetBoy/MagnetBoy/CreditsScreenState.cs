71367c0498f1e78a3e4d132d51838efe3d22263d
Daniel Savage <dansvg@gmail.com>
2013-02-09 19:06:09 -0800

    added credits screen state

---
+++ MagnetBoy/MagnetBoy/MagnetBoy/CreditsScreenState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MagnetBoy
+{
+    public 
+
+    class CreditsScreenState : IState
+    {
+        private ContentManager manager = null;
+
+        public CreditsScreenState(ContentManager newManager)
+        {
+            manager = newManager;
+
+            IsUpdateable = true;
+        }
+
+        protected override void doUpdate(GameTime currentTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
